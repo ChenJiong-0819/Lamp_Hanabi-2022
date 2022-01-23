c7e34cb2f2f575d10b106736046c3ced7db4da23
ChenJiong <1092845896@qq.com>
2022-01-24 00:48:03 +0800

    【状态机】设置状态机

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,258 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, FreeCamera, Color4 } from "@babylonjs/core";
+import { AdvancedDynamicTexture, Button, Control } from "@babylonjs/gui";
+
+enum State { START = 0, GAME = 1, LOSE = 2, CUTSCENE = 3 }
 
 class App {
+    // 通用完整应用程序
+    private _scene: Scene;
+    private _canvas: HTMLCanvasElement;
+    private _engine: Engine;
+
+    // 场景相关
+    private _state: number = 0;
+    private _gamescene: Scene;
+    private _cutScene: Scene;
+
     constructor() {
-        // 创建画布 html 元素并将其附加到网页
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
+        this._canvas = this._createCanvas();
 
         // 初始化巴比伦场景和引擎
-        var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
+        this._engine = new Engine(this._canvas, true);
+        this._scene = new Scene(this._engine);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-
-        // 隐藏/显示检查器
+        // 隐藏/显示检查员
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
             if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
+                if (this._scene.debugLayer.isVisible()) {
+                    this._scene.debugLayer.hide();
                 } else {
-                    scene.debugLayer.show();
+                    this._scene.debugLayer.show();
                 }
             }
         });
 
         // 运行主渲染循环
-        engine.runRenderLoop(() => {
-            scene.render();
+        this._main();
+    }
+
+    private _createCanvas(): HTMLCanvasElement {
+
+        //Commented out for development
+        document.documentElement.style["overflow"] = "hidden";
+        document.documentElement.style.overflow = "hidden";
+        document.documentElement.style.width = "100%";
+        document.documentElement.style.height = "100%";
+        document.documentElement.style.margin = "0";
+        document.documentElement.style.padding = "0";
+        document.body.style.overflow = "hidden";
+        document.body.style.width = "100%";
+        document.body.style.height = "100%";
+        document.body.style.margin = "0";
+        document.body.style.padding = "0";
+
+        // 创建画布html元素并将其附加到网页
+        this._canvas = document.createElement("canvas");
+        this._canvas.style.width = "100%";
+        this._canvas.style.height = "100%";
+        this._canvas.id = "gameCanvas";
+        document.body.appendChild(this._canvas);
+
+        return this._canvas;
+    }
+
+    private async _main(): Promise<void> {
+        await this._goToStart();
+
+        // 注册渲染循环以重复渲染场景
+        this._engine.runRenderLoop(() => {
+            switch (this._state) {
+                case State.START:
+                    this._scene.render();
+                    break;
+                case State.CUTSCENE:
+                    this._scene.render();
+                    break;
+                case State.GAME:
+                    this._scene.render();
+                    break;
+                case State.LOSE:
+                    this._scene.render();
+                    break;
+                default: break;
+            }
         });
+
+        // 如果屏幕已调整大小/旋转，请调整大小
+        window.addEventListener('resize', () => {
+            this._engine.resize();
+        });
+    }
+    private async _goToStart(){
+        this._engine.displayLoadingUI();
+
+        this._scene.detachControl();
+        let scene = new Scene(this._engine);
+        scene.clearColor = new Color4(0,0,0,1);
+        let camera = new FreeCamera("camera1", new Vector3(0, 0, 0), scene);
+        camera.setTarget(Vector3.Zero());
+
+        // 为所有GUI元素创建全屏ui
+        const guiMenu = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        guiMenu.idealHeight = 720; // 将我们的全屏用户界面调整到这个高度
+
+        // 创建一个简单的按钮
+        const startBtn = Button.CreateSimpleButton("start", "PLAY");
+        startBtn.width = 0.2
+        startBtn.height = "40px";
+        startBtn.color = "white";
+        startBtn.top = "-14px";
+        startBtn.thickness = 0;
+        startBtn.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
+        guiMenu.addControl(startBtn);
+
+        // 这将处理与附加到场景的“开始”按钮的交互
+        startBtn.onPointerDownObservable.add(() => {
+            this._goToCutScene();
+            scene.detachControl(); // 禁用的可观测值
+        });
+
+        // --场景加载完毕--
+        await scene.whenReadyAsync();
+        this._engine.hideLoadingUI();
+        // 最后，将当前状态设置为“开始”状态，并将场景设置为“开始”场景
+        this._scene.dispose();
+        this._scene = scene;
+        this._state = State.START;
+    }
+
+    private async _goToCutScene(): Promise<void> {
+        this._engine.displayLoadingUI();
+        // --设置场景--
+        // 加载游戏时，不要检测到此ui的任何输入
+        this._scene.detachControl();
+        this._cutScene = new Scene(this._engine);
+        let camera = new FreeCamera("camera1", new Vector3(0, 0, 0), this._cutScene);
+        camera.setTarget(Vector3.Zero());
+        this._cutScene.clearColor = new Color4(0, 0, 0, 1);
+
+         //--GUI--
+         const cutScene = AdvancedDynamicTexture.CreateFullscreenUI("cutscene");
+
+        // --进展对话--
+        const next = Button.CreateSimpleButton("next", "NEXT");
+        next.color = "white";
+        next.thickness = 0;
+        next.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
+        next.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_RIGHT;
+        next.width = "64px";
+        next.height = "64px";
+        next.top = "-3%";
+        next.left = "-12%";
+        cutScene.addControl(next);
+
+        next.onPointerUpObservable.add(() => {
+            this._goToGame();
+        })
+
+        // --场景加载完成后--
+        await this._cutScene.whenReadyAsync();
+        this._engine.hideLoadingUI();
+        this._scene.dispose();
+        this._state = State.CUTSCENE;
+        this._scene = this._cutScene;
+
+        // --在此场景中开始加载和设置游戏--
+        var finishedLoading = false;
+        await this._setUpGame().then(res =>{
+            finishedLoading = true;
+        });
+    }
+
+    private async _setUpGame() {
+        let scene = new Scene(this._engine);
+        this._gamescene = scene;
+    
+        // ...装载资产
+    }
+
+    private async _goToGame(){
+        // --设置场景--
+        this._scene.detachControl();
+        let scene = this._gamescene;
+        scene.clearColor = new Color4(0.01568627450980392, 0.01568627450980392, 0.20392156862745098); // a color that fit the overall color scheme better
+        let camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.setTarget(Vector3.Zero());
+
+        //--GUI--
+        const playerUI = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        // 加载游戏时，不要检测到此ui的任何输入
+        scene.detachControl();
+
+        // 创建一个简单的按钮
+        const loseBtn = Button.CreateSimpleButton("lose", "LOSE");
+        loseBtn.width = 0.2
+        loseBtn.height = "40px";
+        loseBtn.color = "white";
+        loseBtn.top = "-14px";
+        loseBtn.thickness = 0;
+        loseBtn.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
+        playerUI.addControl(loseBtn);
+
+        // 这将处理与附加到场景的“开始”按钮的交互
+        loseBtn.onPointerDownObservable.add(() => {
+            this._goToLose();
+            scene.detachControl(); // 禁用的可观测值
+        });
+
+        // 临时场景对象
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+
+        // --当场景完成加载时--
+        await scene.whenReadyAsync();
+        // 摆脱开始场景，切换到游戏场景并更改状态
+        this._scene.dispose();
+        this._state = State.GAME;
+        this._scene = scene;
+        this._engine.hideLoadingUI();
+        // 游戏准备好了，请重新连接控制按钮
+        this._scene.attachControl();
+    }
+
+    private async _goToLose(): Promise<void> {
+        this._engine.displayLoadingUI();
+
+        // --场景设置--
+        this._scene.detachControl();
+        let scene = new Scene(this._engine);
+        scene.clearColor = new Color4(0, 0, 0, 1);
+        let camera = new FreeCamera("camera1", new Vector3(0, 0, 0), scene);
+        camera.setTarget(Vector3.Zero());
+
+        //--GUI--
+        const guiMenu = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        const mainBtn = Button.CreateSimpleButton("mainmenu", "MAIN MENU");
+        mainBtn.width = 0.2;
+        mainBtn.height = "40px";
+        mainBtn.color = "white";
+        guiMenu.addControl(mainBtn);
+        // 这将处理与附加到场景的“开始”按钮的交互
+        mainBtn.onPointerUpObservable.add(() => {
+            this._goToStart();
+        });
+
+        // --场景加载完毕--
+        await scene.whenReadyAsync();
+        this._engine.hideLoadingUI(); // 场景准备就绪后，隐藏加载
+        // 最后，将当前状态设置为“丢失”状态，并将场景设置为“丢失”场景
+        this._scene.dispose();
+        this._scene = scene;
+        this._state = State.LOSE;
     }
 }
 new App();
